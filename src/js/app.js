76c24dc6aaff51bd215d3e60292dc56b34b282d3
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-10-06 20:59:42 +0500

    initial

---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+import * as functions from "./modules/functions.js"
+import $ from 'jquery'
+
+functions.isWebp()
+
+// import Swiper, { Navigation, Pagination } from 'swiper'
+
+// const swiper = new Swiper()
+
+const headerButton = document.querySelector(".header__button")
+const headerMenu = document.querySelector(".header__menu")
+const header = document.querySelector(".header")
+let menuOpened = false
+const menuToggle = () => {
+  menuOpened = !menuOpened
+  headerButton.classList.toggle("open")
+  headerMenu.classList.toggle("open")
+  header.classList.toggle("open")
+}
+
+headerButton.onclick = menuToggle
+
+window.onclick = (e) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle()
+}
+
+$(document).ready(function() {
+    $(".accordion > .accordion__button").on("click", function() {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this)
+                .siblings(".accordion__content")
+                .slideUp(200);
+        } else {
+            $(".accordion > .accordion__button").removeClass("active");
+            $(this).addClass("active");
+            $(".accordion__content").slideUp(200);
+            $(this)
+                .siblings(".accordion__content")
+                .slideDown(200);
+        }
+    });
+});
